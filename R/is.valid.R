859f1efc335164fe8166c703ecf31e1660218189
Kleanthis Koupidis <koupidis.okfgr@gmail.com>
2017-08-02 17:52:26 +0300

    portable for other packages and roxygen doc

---
+++ R/is.valid.R
@@ -0,0 +1,94 @@
+#' 
+#' @rdname is.valid
+#' 
+#' @export
+#' 
+
+is.valid = function(descriptor,schema)  {
+  
+  if(jsonlite::validate(descriptor)==TRUE){
+    
+    if(is.null(scheme)){
+      
+      v = jsonvalidate::json_validator(paste(readLines("http://json-schema.org/draft-04/schema#"), collapse=""))
+      
+      
+    } else {
+      #local
+      v = jsonvalidate::json_validator("schema.json")
+    }
+    
+    valid=v(json,verbose = T, greedy=TRUE,error=F)
+    class(valid)="logical"
+    
+    #.print.validator(valid)
+    valid
+  }else message("This is not a valid JSON file.")
+  
+  
+}
+
+#' 
+#' @rdname .print.validator
+#' 
+#' @export
+#' 
+.print.validator = function (x, ...){
+  inherits(x,"logical")
+  cat("This is a valid input descriptor:\n")
+  x
+}
+
+
+
+
+json='{
+  "description": "Country, regional and world GDP in current US Dollars ($). Regional means collections of countries e.g. Europe & Central Asia. Data is sourced from the World Bank and turned into a standard normalized CSV.",
+"image": "http://assets.okfn.org/p/opendatahandbook/img/data-wrench.png",
+"keywords": [
+"GDP",
+"World",
+"Gross Domestic Product",
+"Time series"
+],
+"last_updated": "2011-09-21",
+"license": "PDDL-1.0",
+"name": "gdp",
+"resources": [
+{
+  "name": "gdp",
+  "path": "data/gdp.csv",
+  "schema": {
+  "fields": [
+  {
+  "name": "Country Name",
+  "type": "string"
+  },
+  {
+  "foreignkey": "iso-3-geo-codes/id",
+  "name": "Country Code",
+  "type": "string"
+  },
+  {
+  "format": "any",
+  "name": "Year",
+  "type": "date"
+  },
+  {
+  "description": "GDP in current USD",
+  "name": "Value",
+  "type": "number"
+  }
+  ]
+  }
+}
+],
+"sources": [
+{
+  "name": "World Bank and OECD",
+  "web": "http://data.worldbank.org/indicator/NY.GDP.MKTP.CD"
+}
+],
+"title": "Country, Regional and World GDP (Gross Domestic Product)",
+"version": "2011"
+}'
