27d5070ef06ce77dc0d03b35c8d1544a6a2211a3
Kleanthis Koupidis <koupidis.okfgr@gmail.com>
2017-08-10 20:57:51 +0300

    cast boolean type function

---
+++ R/types.castArray.R
@@ -0,0 +1,17 @@
+#' @title cast array
+#' 
+#' @rdname types.castArray
+#' @export
+#' @description cast array
+#' 
+types.castArray <- function (value) { #format parameter is not used
+  
+ if( !is.array(value) | !is.list(value) | !is.character(value) ) stop("value is not a valid array or list object")
+  
+  tryCatch({
+   #value=jsonlite::fromJSON(value)
+   if (!is.array(value) | !is.list(value) ) stop()
+  },
+  error= function(e) message("value is not a valid array or list object")  )
+  
+}
