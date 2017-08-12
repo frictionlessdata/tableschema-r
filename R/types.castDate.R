f7a2924a6f1e27ca497169ccba940131b3b244f8
Kleanthis Koupidis <koupidis.okfgr@gmail.com>
2017-08-12 05:26:59 +0300

    fix function cast date types

--- R/types.castDate.R
+++ R/types.castDate.R
@@ -6,55 +6,46 @@
 #' 
 types.castDate <- function (format = "%y-%m-%d", value) {
   
-  if ( !is.date(lubridate::as_date(value, format = format)) ) {
+  if ( !lubridate::is.Date(value) ) {
     
-    if( !is.character(value) ) value = stop("Value should be character or date class")
-  
-    tryCatch({
-      if (format == "%y-%m-%d"){
+    if( !is.character(value) ) stop("Value should be character or date class",call. = FALSE)
+    
+    withCallingHandlers(
+      
+      tryCatch({
         
-        value = lubridate::as_date(lubridate::parse_date_time(value, format))
-                
-      } else if (format != "%y-%m-%d") {
         
-        value = lubridate::as_date(value, format) 
-        
-      } else {
-        
-        if (all(startsWith(format,"fmt:")) ){
+        if (format == "%y-%m-%d"){
           
-          warning( sprintf("Format %s is deprecated.\nPlease use '%s' without 'fmt:' prefix.", format, unlist(strsplit(format,":"))[2]) )
+          value = lubridate::as_date(lubridate::parse_date_time(value, format))
+          
+        } else if (startsWith(format,"fmt:") ){
+          
+          warning("Format ",format," is deprecated.\nPlease use ",unlist(strsplit(format,":"))[2]," without 'fmt:' prefix.", call. = FALSE) 
           
           format = trimws(gsub("fmt:","",format),which="both")
+          
+          value = lubridate::as_date(lubridate::parse_date_time(value, format))
+          
+        } else if ( format != "%y-%m-%d" & !startsWith(format,"fmt:") ) {
+          
+          value = lubridate::as_date(value, format) 
+          
         }
         
-        value = lubridate::as_date(lubridate::parse_date_time(value, format))
-      }
-      
-      if (!is.date(lubridate::as_date(value, format = format) )) value =  stop("Value is not a valid date")
+      if ( !lubridate::is.Date(lubridate::as_date(value, format = format)) ) stop("Value is not a valid date",call. = FALSE)
     
-      value = lubridate::as_date(value) 
+      value = lubridate::as_date(value, format) 
       
-      },error = function(e)  err <<- e
-      )
+     },
+      
+      error = function(e)  err <<- e
+     ),
+      
+      warning=function(w) warn <<- w
+     )
+      
   }
   return(value)
 }
 
-
-
-#' Check if date is a valid date
-#' @rdname is.date
-#' @export
-is.date <- function (date, format="%y/%m/%d") {
-  
-  withCallingHandlers(
-    tryCatch(
-    
-    !is.na(lubridate::as_date(lubridate::parse_date_time(date , format ))), error = function(e) {FALSE}
-    )  ,warning=function(w) {
-             warn <<- w
-             invokeRestart("muffleWarning")
-    })
-  
-}
