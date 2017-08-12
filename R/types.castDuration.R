39bc1a273f75ed19fa68018d9eb9f57e4d2a4fe2
Kleanthis Koupidis <koupidis.okfgr@gmail.com>
2017-08-12 18:40:47 +0300

    function cast duration type

---
+++ R/types.castDuration.R
@@ -0,0 +1,26 @@
+#' @title cast duration
+#' 
+#' @rdname types.castDuration
+#' @export
+#' @description cast duration
+#' 
+
+types.castDuration <- function (value) { #format parameter is not used   
+  
+  if (!lubridate::is.duration(value)) {
+    
+    if (!is.character(value)) stop("Value should be character or date class",call. = FALSE)
+    
+    #tryCatch({
+      
+    value=lubridate::as.duration(value)
+    
+    #if( not in milliseconds()) stop("Value should be in milliseconds",call. = FALSE)
+    
+    #},error= function(e) err<<-e)
+    
+    return(value)
+    
+  }
+}
+
