39e2dde83ff590d047afb3048b5267f52dc7e9a1
Kleanthis Koupidis <koupidis.okfgr@gmail.com>
2017-09-14 19:09:22 +0300

    add cast "any" type

---
+++ R/types.castAny.R
@@ -0,0 +1,10 @@
+#' @title cast any
+#' @description cast any
+#' @param value value
+#' @rdname types.castAny
+#' @export
+#' 
+
+types.castAny <- function (value) { #format is not used
+  return (value)
+}
